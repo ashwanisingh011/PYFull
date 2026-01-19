3ffdd9ddc6c38e68adf6e40a94ab06712f10b852
ashwanisingh011 <ashwanisingh9737@gmail.com>
2026-01-19 22:29:42 +0530

    List learning

--- list.py
+++ list.py
@@ -1,7 +1,20 @@
-l1 = [1,2,3]
-l2 = l1
-print(l1)
-print(l2)
-l1[0] = 33
-print(l1)
-print(l2)
+tea_varities = ["Black", "Green", "Oolong", "White"]
+print(tea_varities)
+
+print(tea_varities[0])
+
+print(tea_varities[-1])
+
+print(tea_varities[1:3])
+
+tea_varities[3] = "Herbal"
+print(tea_varities)
+
+tea_varities[1:2] = "Lemon"
+print(tea_varities) 
+
+tea_varities[1:2] = ["Lemon"]
+print(tea_varities)
+
+for tea in tea_varities:
+    print(tea, end="-")
