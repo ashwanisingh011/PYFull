c3906fb6f4980aa43df4181536fafe92207f086a
ashwanisingh011 <ashwanisingh9737@gmail.com>
2026-01-30 16:59:50 +0530

    scopes learning

---
+++ Scopes/scope.py
@@ -0,0 +1,18 @@
+username = "Viola"
+
+def func():
+    username = "Ashwani"
+    print(username)
+
+
+print(username)
+func()
+
+x = 99
+
+def func2(y):
+    z = x + y
+    return z 
+
+result = func2(1)
+print(result)
