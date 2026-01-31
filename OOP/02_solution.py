cf0ace16387984e43eac0f2931773d7788f92f06
ashwanisingh011 <ashwanisingh9737@gmail.com>
2026-01-31 19:05:16 +0530

    added a method to the class

---
+++ OOP/02_solution.py
@@ -0,0 +1,14 @@
+# 2. Class method and Self
+# Problem: Add a method to the car class that displays the full name of the car (brand and model)
+
+class Car:
+    def __init__ (self, brand, model):
+        self.brand = brand
+        self.model = model
+    def fullname(self):
+        return f"{self.brand} {self.model}"
+
+my_car = Car("Toyota", "Fortuner")
+print(my_car.brand)
+print(my_car.model)
+print(my_car.fullname())
