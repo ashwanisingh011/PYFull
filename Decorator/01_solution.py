f78ef3bf5e2488aec0850d396fff0275d4f9f180
ashwanisingh011 <ashwanisingh9737@gmail.com>
2026-02-02 22:45:46 +0530

    Decorator

--- Decorator/01_solution.py
+++ Decorator/01_solution.py
@@ -1,1 +1,13 @@
 # Timing function execution 
+# Write a decorator that mesures the time a function takes to execute
+
+import time
+
+def timing_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Execution time: {end_time - start_time} seconds")
+        return result
+    return wrapper
