04613b31b5a475f3db7942a5d1012d058cf8f010
ashwanisingh011 <ashwanisingh9737@gmail.com>
2026-02-26 23:54:18 +0530

    insertion sort

--- Project/BubbleSort.py
+++ Project/BubbleSort.py
@@ -7,3 +7,18 @@ for i in range(n):
             arr[j], arr[j+1] = arr[j+1], arr[j]
 
 print(arr)
+
+
+# Insertion Sort
+
+arr = [3,4,7,8,2,1]
+n = len(arr)
+for i in range(1, n):
+    key = arr[i]
+    j = i-1
+    while j >= 0 and key < arr[j]:
+        arr[j+1] = arr[j]
+        j -= 1
+    arr[j+1] = key
+
+print(arr) 
